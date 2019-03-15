54261f5e7a0df63323a172f041830dafd678cb19
aldrichvalentino <aldrich.vh97@gmail.com>
2019-03-15 11:24:40 +0700

    Fixed retain controller

--- src/main/java/core/RetainController.java
+++ src/main/java/core/RetainController.java
@@ -11,6 +11,12 @@ import es.ucm.fdi.gaia.jcolibri.cbrcore.CBRCase;
 import es.ucm.fdi.gaia.jcolibri.cbrcore.CBRCaseBase;
 import es.ucm.fdi.gaia.jcolibri.cbrcore.Connector;
 import model.CaseBase;
+import model.ControlButtons;
+import model.FormDescription;
+import model.FormSolution;
+import model.InputFields;
+import model.OutputFields;
+import model.RevisedModel;
 import utils.DatabaseConnector;
 
 @Controller
@@ -29,16 +35,55 @@ public class RetainController {
 
             caseBase.init(connector);
 
+            // manualy convert from CBRCase to string, then to CBRCase again
+            CBRCase revisedCase = adjustCase(AdaptationController.adaptedCase);
+
             // retain case
-            System.out.println("mo retain");
-            System.out.println(AdaptationController.adaptedCase);
             Collection<CBRCase> cases = new HashSet<CBRCase>();
-            cases.add(AdaptationController.adaptedCase);
-            caseBase.learnCases(cases); // FIXME: this still cause error
+            cases.add(revisedCase);
+            caseBase.learnCases(cases);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return "retainResult";
+    }
+
+    private CBRCase adjustCase(CBRCase _case) {
+        CBRCase revisedCase = _case;
+        FormDescription description = new FormDescription();
+        FormSolution solution = new FormSolution();
+        RevisedModel revisedModel = new RevisedModel();
+        try {
+            revisedModel.setFormName(((FormDescription) revisedCase.getDescription()).getFormName());
+            revisedModel.setInputFields(((FormDescription) revisedCase.getDescription()).itoString());
+            revisedModel.setOutputFields(((FormDescription) revisedCase.getDescription()).otoString());
+            revisedModel.setControlButtons(((FormDescription) revisedCase.getDescription()).ctoString());
+            revisedModel.setLabels(((FormSolution) revisedCase.getSolution()).ltoString());
+            revisedModel.setGroups(((FormSolution) revisedCase.getSolution()).gtoString());
+            revisedModel.setOrders(((FormSolution) revisedCase.getSolution()).otoString());
+            revisedModel.setvLayouts(((FormSolution) revisedCase.getSolution()).vltoString());
+            revisedModel.sethLayouts(((FormSolution) revisedCase.getSolution()).hltoString());
+
+            description.setId((int) revisedCase.getID());
+            description.setFormName(revisedModel.getFormName());
+            description.setInputFields(revisedModel.addDesc(InputFields.class, revisedModel.getInputFields()));
+            description.setOutputFields(revisedModel.addDesc(OutputFields.class, revisedModel.getOutputFields()));
+            description.setControlButtons(revisedModel.addDesc(ControlButtons.class, revisedModel.getControlButtons()));
+
+            solution.setId((int) revisedCase.getID());
+            solution.setlabel(revisedModel.addLabel(revisedModel.getLabels()));
+            solution.setGroup(revisedModel.addGroup(revisedModel.getGroups()));
+            solution.setOrder(revisedModel.addOrder(revisedModel.getOrders()));
+            solution.setvlMember(revisedModel.addVL(revisedModel.getvLayouts()));
+            solution.sethlMember(revisedModel.addHL(revisedModel.gethLayouts()));
+
+            revisedCase.setDescription(description);
+            revisedCase.setSolution(solution);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return revisedCase;
     }
 }
--- src/main/java/utils/DatabaseConnector.java
+++ src/main/java/utils/DatabaseConnector.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -18,9 +20,19 @@ import es.ucm.fdi.gaia.jcolibri.cbrcore.CaseBaseFilter;
 import es.ucm.fdi.gaia.jcolibri.cbrcore.CaseComponent;
 import es.ucm.fdi.gaia.jcolibri.cbrcore.Connector;
 import es.ucm.fdi.gaia.jcolibri.exception.InitializingException;
+import model.ControlButtons;
 import model.FormDescription;
 import model.FormSolution;
 import model.Forms;
+import model.GMembers;
+import model.Groups;
+import model.HLMembers;
+import model.InputFields;
+import model.OMembers;
+import model.Orders;
+import model.OutputFields;
+import model.VLMembers;
+import model.XLabel;
 
 public class DatabaseConnector implements Connector {
     private static DatabaseConnector connector = null;
@@ -31,17 +43,13 @@ public class DatabaseConnector implements Connector {
     SessionFactory sessionFactory;
 
     final String FORMS_PATH = getClass().getResource("/database/Form.hbm.xml").toExternalForm();
-    final String FORM_DESCRIPTION_PATH =
-            getClass().getResource("/database/FormDescription.hbm.xml").toExternalForm();
-    final String FORM_SOLUTION_PATH =
-            getClass().getResource("/database/FormSolution.hbm.xml").toExternalForm();
+    final String FORM_DESCRIPTION_PATH = getClass().getResource("/database/FormDescription.hbm.xml").toExternalForm();
+    final String FORM_SOLUTION_PATH = getClass().getResource("/database/FormSolution.hbm.xml").toExternalForm();
 
-    private DatabaseConnector(String driver, String connection, String dialect, String username,
-            String password) {
+    private DatabaseConnector(String driver, String connection, String dialect, String username, String password) {
         try {
             cfg = new Configuration().setProperty("hibernate.connection.driver_class", driver)
-                    .setProperty("hibernate.connection.url", connection)
-                    .setProperty("hibernate.dialect", dialect)
+                    .setProperty("hibernate.connection.url", connection).setProperty("hibernate.dialect", dialect)
                     .setProperty("hibernate.connection.username", username)
                     .setProperty("hibernate.connection.password", password);
             cfg.addResource(FORM_DESCRIPTION_PATH);
@@ -53,8 +61,8 @@ public class DatabaseConnector implements Connector {
         }
     }
 
-    public static DatabaseConnector getInstance(String driver, String conn, String dialect,
-            String username, String password) {
+    public static DatabaseConnector getInstance(String driver, String conn, String dialect, String username,
+            String password) {
         if (connector == null) {
             connector = new DatabaseConnector(driver, conn, dialect, username, password);
         }
@@ -77,21 +85,17 @@ public class DatabaseConnector implements Connector {
 
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        for (CBRCase frmi : cases) {
-            Forms forms = new Forms();
-            forms.setId((int) frmi.getID());
-
-            HashSet<FormDescription> hd = new HashSet<FormDescription>();
-            hd.add((FormDescription) frmi.getDescription());
-            forms.setFormDes(hd);
-
-            HashSet<FormSolution> hs = new HashSet<FormSolution>();
-            hs.add((FormSolution) frmi.getSolution());
-            forms.setFormSol(hs);
-
-            session.save(forms);
+        try {
+            for (CBRCase frmi : cases) {
+                Forms forms = setToZero((int) frmi.getID(), (FormDescription) frmi.getDescription(),
+                        (FormSolution) frmi.getSolution());
+                session.save(forms);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
         }
-        transaction.commit();
         session.close();
     }
 
@@ -115,8 +119,7 @@ public class DatabaseConnector implements Connector {
             transaction = session.beginTransaction();
 
             solList = new HashMap<Object, CaseComponent>();
-            List list = session.createQuery(String.format("from %s", FormSolution.class.getName()))
-                    .list();
+            List list = session.createQuery(String.format("from %s", FormSolution.class.getName())).list();
 
             transaction.commit();
             session.close();
@@ -128,8 +131,7 @@ public class DatabaseConnector implements Connector {
 
             session = sessionFactory.openSession();
             transaction = session.beginTransaction();
-            descList = session
-                    .createQuery(String.format("from %s", FormDescription.class.getName())).list();
+            descList = session.createQuery(String.format("from %s", FormDescription.class.getName())).list();
             transaction.commit();
             session.close();
 
@@ -172,6 +174,88 @@ public class DatabaseConnector implements Connector {
     @Override
     public Collection<CBRCase> retrieveSomeCases(CaseBaseFilter filter) {
         return null;
+    }
+
+    private Forms setToZero(int id, FormDescription formDescription, FormSolution formSolution) {
+        Forms newForm = new Forms();
+        FormDescription newFormDescription = new FormDescription();
+        FormSolution newFormSolution = new FormSolution();
+
+        Set<InputFields> inputFields = new HashSet<InputFields>();
+        for (InputFields inputField : formDescription.getInputFields()) {
+            inputFields.add(new InputFields(inputField.getName()));
+        }
+
+        Set<OutputFields> outputFields = new HashSet<OutputFields>();
+        for (OutputFields outputField : formDescription.getOutputFields()) {
+            outputFields.add(new OutputFields(outputField.getName()));
+        }
+
+        Set<ControlButtons> controlButtons = new HashSet<ControlButtons>();
+        for (ControlButtons controlButton : formDescription.getControlButtons()) {
+            controlButtons.add(new ControlButtons(controlButton.getName()));
+        }
+
+        // newFormDescription.setId(id);
+        newFormDescription.setFormName(formDescription.getFormName());
+        newFormDescription.setInputFields(inputFields);
+        newFormDescription.setOutputFields(outputFields);
+        newFormDescription.setControlButtons(controlButtons);
+        Set<FormDescription> setFormDescription = new HashSet<FormDescription>();
+        setFormDescription.add(newFormDescription);
+        newForm.setFormDes(setFormDescription);
+
+        Map<String, XLabel> labels = formSolution.getlabel();
+        for (Map.Entry<String, XLabel> label : labels.entrySet()) {
+            label.setValue(new XLabel(label.getValue().getLabel()));
+        }
+
+        List<Groups> groups = new ArrayList<Groups>();
+        for (Groups group : formSolution.getGroup()) {
+            Groups g = new Groups();
+            Set<GMembers> gMembers = new HashSet<GMembers>();
+            for (GMembers gMember : group.getgMembers()) {
+                gMembers.add(new GMembers(gMember.getName()));
+            }
+            g.setgMembers(gMembers);
+            groups.add(g);
+        }
+
+        List<Orders> orders = new ArrayList<Orders>();
+        for (Orders group : formSolution.getOrder()) {
+            Orders o = new Orders();
+            List<OMembers> oMembers = new ArrayList<OMembers>();
+            for (OMembers oMember : group.getoMembers()) {
+                oMembers.add(new OMembers(oMember.getMemberName()));
+            }
+            o.setoMembers(oMembers);
+            orders.add(o);
+        }
+
+        List<HLMembers> hLMembers = new ArrayList<HLMembers>();
+        for (HLMembers hLMember : formSolution.gethlMember()) {
+            hLMembers.add(new HLMembers(hLMember.getMemberName()));
+        }
+
+        List<VLMembers> vLMembers = new ArrayList<VLMembers>();
+        for (VLMembers vLMember : formSolution.getvlMember()) {
+            vLMembers.add(new VLMembers(vLMember.getName()));
+        }
+
+        // newFormSolution.setId(id);
+        newFormSolution.setlabel(labels);
+        newFormSolution.setGroup(groups);
+        newFormSolution.setOrder(orders);
+        newFormSolution.sethlMember(hLMembers);
+        newFormSolution.setvlMember(vLMembers);
+        Set<FormSolution> setFormSolution = new HashSet<FormSolution>();
+        setFormSolution.add(newFormSolution);
+        newForm.setFormSol(setFormSolution);
+
+        System.out.println(newFormDescription);
+        System.out.println(newFormSolution);
+
+        return newForm;
     }
 
 }
